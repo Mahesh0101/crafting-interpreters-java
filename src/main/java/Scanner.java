fe6081dcc06e26236253265311259cc93990c36f
Mahesh0101 <maheshbabuv8321@gmail.com>
2025-05-04 18:02:27 +0530

    refactoring

---
+++ src/main/java/Scanner.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class Scanner {
+    private final String source;
+    ArrayList<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    public Scanner(String source) {
+        this.source = source;
+    }
+    
+    public ArrayList<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+        // System.out.println(start + "  " + current);
+        tokens.add(new Token(TokenType.EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken(){
+        char c = getCurrentChar();
+        switch (c) {
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void addToken(TokenType token){
+        addToken(token, null);
+    }
+
+    private void addToken(TokenType token, Object literal){
+        String lexeme = source.substring(start, current);
+        tokens.add(new Token(token, lexeme, literal, line));
+    }
+
+    private boolean isAtEnd()
+    {
+        return current >= source.length();
+    }
+    private char getCurrentChar()
+    {
+        return source.charAt(current++);
+    }
+}
